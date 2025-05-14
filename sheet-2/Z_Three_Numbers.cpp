a110d165b5b2cedccf5e56e28df72fc723a2ac02
a7med-830 <ahmedelshekih830@gmail.com>
2025-05-14 13:35:39 +0300

    Z

---
+++ sheet-2/Z_Three_Numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, s, c = 0;
+    cin >> k >> s;
+
+    for (int i = 0; i <= k; i++) {
+        for (int j = 0; j <= k; j++) {
+            if (s - i - j >= 0 && s - i - j <= k)
+                c++;
+        }
+    }
+
+    cout << c << endl;
+}
