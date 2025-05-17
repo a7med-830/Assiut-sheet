63193ba6716072eb8ae413ee4182a9db5e7e4d61
a7med-830 <ahmedelshekih830@gmail.com>
2025-05-17 10:25:31 +0300

    K

---
+++ sheet-3/K_Sum_Digits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    string digits;
+    cin >> digits;
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += digits[i] - '0';
+    }
+
+    cout << sum;
+}
