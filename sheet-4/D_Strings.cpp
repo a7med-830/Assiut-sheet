61877436071daba84365890f07a9f17d985ebfa7
a7med-830 <ahmedelshekih830@gmail.com>
2025-06-11 20:25:27 +0300

    D

---
+++ sheet-4/D_Strings.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string x, y;
+    cin >> x >> y;
+
+    cout << x.length() << " " << y.length() << endl;
+    cout << x << y << endl;
+    cout << y[0] << x.substr(1, x.length()) << " " << x[0] << y.substr(1, y.length());
+}
