d278da236a504e356ba3b36d4ca1a00ba13a9927
a7med-830 <ahmedelshekih830@gmail.com>
2025-06-11 19:34:19 +0300

    A

---
+++ sheet-4/A_Create_A_New_String.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+
+    cout << s.length() << " " << t.length() << endl;
+    cout << s << " " << t;
+}
