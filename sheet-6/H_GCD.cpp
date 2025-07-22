51c6da34e56c844655fd2174babf214b379f9eac
a7med-830 <ahmedelshekih830@gmail.com>
2025-07-22 15:50:52 +0300

    H

---
+++ sheet-6/H_GCD.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+ll gcd(ll a, ll b) {
+    while (b != 0) {
+        ll temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+
+int main() {
+    ll a, b;
+    cin >> a >> b;
+
+    cout << gcd(a, b) << " " << lcm(a, b) << endl;
+}
