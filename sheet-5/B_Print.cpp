bc61ec02bf755cc54442f49449d19325640ea282
a7med-830 <ahmedelshekih830@gmail.com>
2025-07-24 00:07:07 +0300

    B

---
+++ sheet-5/B_Print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+void print(int n) {
+    for (int i = 1; i < n; i++)
+        cout << i << " ";
+        cout << n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    print(n);
+}
