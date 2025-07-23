bbbd4e579f3637f5e371e17b438f3c204b7b9dee
a7med-830 <ahmedelshekih830@gmail.com>
2025-07-23 23:56:27 +0300

    A

---
+++ sheet-5/A_Add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+int sum(int a, int b) { return a + b; }
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    cout << sum(a,b);
+}
