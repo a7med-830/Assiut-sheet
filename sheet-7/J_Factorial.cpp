00af665aff92b022e003932ebe6093aa5f5d72b6
a7med-830 <ahmedelshekih830@gmail.com>
2025-07-31 23:45:04 +0300

    J

--- sheet-7/J_Factorial.cpp
+++ sheet-7/J_Factorial.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 typedef long long ll;
 
-int factorial(int n) {
-    if (n == 0 || n == 1) 
+ll factorial(ll n) {
+    if (n == 0 || n == 1)
         return 1;
-    else
-        return n * factorial(n - 1); 
+    return n * factorial(n - 1);
 }
 
 int main() {
-    int n;
+    ll n;
     cin >> n;
-    cout << factorial(n);
+    cout << factorial(n) << endl;
 }
