9c80b0f3731c5a7aa4ee924a909f71f8a8952ae8
a7med-830 <ahmedelshekih830@gmail.com>
2025-07-31 17:28:43 +0300

    A

---
+++ sheet-7/A_Print_Recursion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+typedef long long ll;
+
+int print(int &n) {
+    if (n == 0) {
+        return 0;
+    }
+    cout << "I love Recursion\n";
+    n--;
+    return print(n);
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    print(n);
+}
