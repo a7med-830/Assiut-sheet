7a9e567691ce570c65e7850daa59dbaba27b5d73
a7med-830 <ahmedelshekih830@gmail.com>
2025-08-03 10:50:45 +0300

    Z

--- sheet-4/Z_Clean_Code.cpp
+++ sheet-4/Z_Clean_Code.cpp
@@ -1,11 +1,33 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
 using namespace std;
+typedef long long ll;
 
 int main() {
-    string s;
-    while (cin >> s) {
-        if (s.find)
+    string line;
+    int start = 1, flag = 0;
+
+    while (getline(cin, line)) {
+
+        flag = 0;
+        if (line.size() == 0 || line == " ") continue;
+
+        for (int i = 0; i < line.size(); i++) {
+
+            if (line[i] == '/' && line[i + 1] == '/' && start) break;
+            else if (line[i] == '/' && line[i + 1] == '*') {
+                start = 0;
+                i++;
+            }
+            else if (line[i] == '*' && line[i + 1] == '/' && !start) {
+                start = 1;
+                i++;
+            }    
+            else if (start) {
+                cout << line[i];
+                flag = 1;
+            }
+
+        }
+        if(flag && start) cout << endl;
     }
 }
