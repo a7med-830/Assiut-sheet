0aed25252e10ae39c381045120557c07db07cd53
a7med-830 <ahmedelshekih830@gmail.com>
2025-08-27 20:24:40 +0300

    W

---
+++ general-easy/W_Mahmoud_and_Longest_Uncommon_Subsequence.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    if (a == b) cout << -1;
+    else cout << max(a.length(), b.length());
+}
