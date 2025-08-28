3b3f5e15bb71f2c9074650aef7a38f50c3384d33
a7med-830 <ahmedelshekih830@gmail.com>
2025-08-28 19:59:34 +0300

    K

---
+++ general-medium/K_Stars_Drawing_Easy_Edition.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    char space[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++)
+            cin >> space[i][j];
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+
+        }
+    }
+}
