d1634b38403b695c6895f7ffd0c0fb16686a951d
a7med-830 <ahmedelshekih830@gmail.com>
2025-08-31 15:05:04 +0300

    A

---
+++ general-medium/A_Palindromes_Replace.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int n = s.length();
+
+    for (int i = 0; i < n / 2; i++) {
+        int j = n - 1 - i;
+        if (s[i] == '?' && s[j] == '?') {
+            s[i] = s[j] = 'a';
+        }
+        else if (s[i] == '?') {
+            s[i] = s[j];
+        }
+        else if (s[j] == '?') {
+            s[j] = s[i];
+        }
+        else if (s[i] != s[j]) {
+            cout << -1;
+            return 0;
+        }
+    }
+
+    if (n % 2 == 1 && s[n / 2] == '?') {
+        s[n / 2] = 'a';
+    }
+
+    cout << s;
+}
