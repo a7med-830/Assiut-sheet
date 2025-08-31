e69388cc27f2be595101ab2df83d0823c377e62c
a7med-830 <ahmedelshekih830@gmail.com>
2025-08-31 15:01:43 +0300

    Z

--- general-medium/Z_Fraction.cpp
+++ general-medium/Z_Fraction.cpp
@@ -3,5 +3,10 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    ll a, b, c, d;
+    char c0;
+
+    cin >> a >> c0 >> b >> c >> c0 >> d;
     
+    cout << lcm(a,c) << "/" << __gcd(b,d) << endl;
 }
