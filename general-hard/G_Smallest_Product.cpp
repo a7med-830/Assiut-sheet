6ab6be45fefbfa8c582c971526e2b47aad17f344
a7med-830 <ahmedelshekih830@gmail.com>
2025-09-02 23:19:47 +0300

    G

--- general-hard/G_Smallest_Product.cpp
+++ general-hard/G_Smallest_Product.cpp
@@ -3,23 +3,14 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    int n; 
-    cin >> n;
-    
-    vector<int> a(n);
-    ll product = 1;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-        product *= a[i];
+    ll s;
+    cin >> s;
+    double sum = 0;
+    for (int i = 0; i < s; i++) {
+        ll product;
+        cin >> product;
+        sum += log10(product);
     }
-    
-    sort(a.begin(), a.end());
-    
-    for (int i = 0; i < n; i++) {
-        if (pow(a[i], n) > product) {
-            cout << a[i] << endl;
-            return 0;
-        }
-    }
-
+    double res = sum / s;
+    cout << (ll)pow(10, res) + 1;
 }
