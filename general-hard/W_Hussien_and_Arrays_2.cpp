98e952b06b9d655f547b006544f1ea900ce89a02
a7med-830 <ahmedelshekih830@gmail.com>
2025-09-07 15:37:12 +0300

    W

---
+++ general-hard/W_Hussien_and_Arrays_2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define fast cin.tie(0); cout.sync_with_stdio(0);
+
+int main() {
+    fast 
+    int size, res = -1;
+    cin >> size;
+    int arr[size];
+    priority_queue<pair<int, int>> p;
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+        p.push({arr[i], i});
+    }
+    for (int i = 0; i < size; i++) {
+        while (p.size() > 0 && p.top().first >= arr[i]) {
+            res = max(res, p.top().second - i);
+            p.pop();
+        }
+    }
+    cout << res;
+}
