3c719d52c8f2562f299ac8d20b483ec2b28800cb
a7med-830 <ahmedelshekih830@gmail.com>
2025-09-07 15:37:07 +0300

    U

--- general-hard/U_From_S_To_T.cpp
+++ general-hard/U_From_S_To_T.cpp
@@ -3,5 +3,39 @@ using namespace std;
 typedef long long ll;
 
 int main() {
+    int t; 
+    cin >> t;
+
+    stop:
+
+    while (t--) {
+        string a, b, c;
+        cin >> a >> b >> c;
+
+        int arr[500] = {0};
+        for (auto ch : c) arr[ch]++;
+
+        int index = 0, k = 0;
+        for (auto ch : b) {
+            if (a[index] != ch) {
+                arr[ch]--;
+                k++;
+            }
+            else {
+                index++;
+            } 
+
+            if (arr[ch] < 0) {
+                cout << "NO\n";
+                goto stop;
+            }
+        }
+        
+        if (a.length() + k == b.length()) 
+            cout << "YES\n";
+        else 
+            cout << "NO\n";
+
+    }
     
 }
