08e2a603a7256394afa238bbacb8855cb34bc7f7
a7med-830 <ahmedelshekih830@gmail.com>
2025-09-07 15:37:45 +0300

    C

---
+++ MET-sheets/sheet-11/C_Swap.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main() {
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+
+    cout << a[2] << " " << a[0] << " " << a[1];
+}
