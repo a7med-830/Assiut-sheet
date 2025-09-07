fc2cb3065dc9887350fc9f6051d68737f8e91a87
a7med-830 <ahmedelshekih830@gmail.com>
2025-09-07 15:36:55 +0300

    P

--- general-hard/P_No_Time_for_Dragons.cpp
+++ general-hard/P_No_Time_for_Dragons.cpp
@@ -3,21 +3,21 @@ using namespace std;
 typedef long long ll;
 
 int main() {
-    long long size;
-    cin >> size;
-    vector<pair<long, pair<long, long>>> v(size);
+    ll s;
+    cin >> s;
+    vector<pair<long, pair<long, long>>> v(s);
 
-    for (int i = 0; i < size; i++) {
-        long long n1, n2;
+    for (int i = 0; i < s; i++) {
+        ll n1, n2;
         cin >> n1 >> n2;
         v[i].first = (n1 - n2);
         v[i].second = {n1, n2};
     }
     sort(v.begin(), v.end());
-    long long total = v[size - 1].second.first;
-    long long sol = v[size - 1].second.first - v[size - 1].second.second;
+    ll total = v[s - 1].second.first;
+    ll sol = v[s - 1].second.first - v[s - 1].second.second;
 
-    for (int i = size - 2; i >= 0; i--) {
+    for (int i = s - 2; i >= 0; i--) {
         if (sol < v[i].second.first) {
             total += v[i].second.first - sol;
             sol = v[i].second.first;
